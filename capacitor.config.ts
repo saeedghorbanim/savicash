b1a00ef557912d990a1e3719e8473eecbcad3ecb
Saeed Ghorbani Moghadam <sghorbani@MacBookPro.lan>
2026-01-11 03:23:38 -0600

    changes with xcode needed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.savicash.app',
   appName: 'savicash',
-  webDir: 'dist',
-  server: {
-    url: 'https://472a9dd8-7da3-4013-8c38-c3a88cbefba1.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
